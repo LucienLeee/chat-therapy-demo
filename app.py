a32aa7109e5495c6515c98e7b6b9e04bc9eafae4
MagicTube <84670116+LucienLeee@users.noreply.github.com>
2024-05-31 13:40:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 import os
-os.system("python download...")
-os.system('streamlit run demo_chat... --server.address=0.0.0.0 --server.port 7860')
+os.system("python demo_download.py")
+os.system('streamlit run demo_chat.py --server.address=0.0.0.0 --server.port 7860')

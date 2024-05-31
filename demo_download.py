754cdc6adb4ef8c3c216c05143272d7e9a0411c7
MagicTube <84670116+LucienLeee@users.noreply.github.com>
2024-05-31 14:32:28 +0800

    Update demo_download.py

--- demo_download.py
+++ demo_download.py
@@ -4,5 +4,5 @@ base_path = './chat-therapy'
 # download repo to the base_path directory using git
 os.system('apt install git')
 os.system('apt install git-lfs')
-os.system(f'git clone https://code.openxlab.org.cn/LucienJMLee/chat-therapy.git {base_path}')
+os.system(f'git clone -b InternLM7b-base https://code.openxlab.org.cn/LucienJMLee/chat-therapy.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')

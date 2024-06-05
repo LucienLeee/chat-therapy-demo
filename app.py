d57f5b258e0318fbb6de00e63ede034fe29beef2
MagicTube <84670116+LucienLeee@users.noreply.github.com>
2024-06-05 15:47:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 import os
 os.system("python demo_download.py LucienJMLee/chat-therapy")
-os.system('streamlit run demo_chat.py --server.address=0.0.0.0 --server.port 7860')
+os.system('streamlit run demo_chat.py --server.address=0.0.0.0 --server.port 6006')
